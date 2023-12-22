5070733942942592510f8172452cffce8ca2165c
jeong-yeji <jeongyeji.mail@gmail.com>
2023-12-22 20:24:21 +0900

    필드 이름 바꾸기(Rename Field)

---
+++ src/main/java/me/whiteship/refactoring/_01_smell_mysterious_name/_03_rename_field/StudyReview.java
@@ -0,0 +1,6 @@
+package me.whiteship.refactoring._01_smell_mysterious_name._03_rename_field;
+
+// constructor, getter, setter, hashcode, equals, toString 자동 생성
+public record StudyReview(String reviewer, String review) {
+
+}
